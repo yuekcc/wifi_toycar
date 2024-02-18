9ded1e0812a7a51819cae15663d2a2199411c794
yuekcc <yuekcc@qq.com>
2024-02-18 23:50:36 +0800

    styles

--- apps/websrv/main.py
+++ apps/websrv/main.py
@@ -1,45 +1,46 @@
 import network
 import usocket as socket
 
-response_headers = b'''
+response_headers = b"""
 HTTP/1.1 200 OK
 Content-Type: text/html
 Connection: close
 
-'''
+"""
 
-content = b'''
+content = b"""
 Hello world
-'''
+"""
 
 # setup wifi
 ap = network.WLAN(network.AP_IF)
-ap.config(essid='lalala', authmode=4, password='12345678')
+ap.config(essid="lalala", authmode=4, password="12345678")
 ap.active(True)
+
 
 def main():
     # 设置 socket 模式
     s = socket.socket()
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # 端口用完后立即释放
-    s.bind(socket.getaddrinfo('0.0.0.0', 80)[0][-1]) # 绑定端口
-    s.listen(5) # 最大连接线 5
-    
-    print('lalala ready, hosted on', ap.ifconfig()[0])
-    print('')
-    
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # 端口用完后立即释放
+    s.bind(socket.getaddrinfo("0.0.0.0", 80)[0][-1])  # 绑定端口
+    s.listen(5)  # 最大连接线 5
+
+    print("lalala ready, hosted on", ap.ifconfig()[0])
+    print("")
+
     while True:
         client_sock, client_addr = s.accept()
-        print('client connected, addr =', client_addr)
-        
+        print("client connected, addr =", client_addr)
+
         while True:
             h = client_sock.readline()
-            print(h.decode('utf8'), end='')
-            if h == b'' or h == b'\r\n':
+            print(h.decode("utf8"), end="")
+            if h == b"" or h == b"\r\n":
                 break
-        
+
         client_sock.write(response_headers)
         client_sock.write(content)
         client_sock.close()
 
-main()
 
+main()
